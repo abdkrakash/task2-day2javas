24e595571050dbd046a053ed23035d616538ea5d
abdkrakash <ABDKARKASH@GMAIL.COM>
2024-12-30 12:32:23 -0800

    Add files via upload

---
+++ task11.js
@@ -0,0 +1,38 @@
+// var GPA = 'A';  
+
+// switch (GPA) {
+//     case 'A':
+//         document.write("excellent");
+//         break;
+//     case 'B':
+//         document.write("very good");
+//         break;
+//     case 'C':
+//         document.write("good");
+//         break;
+//     case 'F':
+//         document.write("failed");
+//         break;
+//     default:
+//         document.write("invalid GPA");
+// }
+
+
+var GPA = 'f';  
+
+switch (GPA) {
+    case 'a':
+        document.write("excellent");
+        break;
+    case 'b':
+        document.write("very good");
+        break;
+    case 'c':
+        document.write("good");
+        break;
+    case 'f':
+        document.write("failed");
+        break;
+    default:
+        document.write("invalid GPA");
+}
---
+++ task7.js
@@ -0,0 +1,7 @@
+var list = [1, 2, 3];
+
+if (Array.isArray(list)) {
+    document.write("It's an array");
+} else {
+    document.write("It's not an array");
+}
